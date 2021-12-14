f99073eb54e1beca1eb96a0c54381344c6981ac3
Timothy Wikedzi <timothy.wikedzi@andela.com>
2021-12-14 23:19:31 +0300

    add payload column on table

--- resources/views/flipgrids/index.blade.php
+++ resources/views/flipgrids/index.blade.php
@@ -2,11 +2,13 @@
     <tr>
         <td>Grid ID</td>
         <td>Topic ID</td>
+        <td>Payload</td>
     </tr>
     @foreach($flipgrids as $flipgrid)
         <tr>
             <td>{{ $flipgrid->grid_id }}</td>
             <td>{{ $flipgrid->topic_id }}</td>
+            <td>{{ $flipgrid->payload }}</td>
         </tr>
     @endforeach
 </table>
