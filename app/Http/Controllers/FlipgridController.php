210d117058cb255aced5628e29c35de38a2e9150
Timothy Wikedzi <timothy.wikedzi@andela.com>
2022-01-13 14:00:57 +0300

    implement request processing function

--- app/Http/Controllers/FlipgridController.php
+++ app/Http/Controllers/FlipgridController.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Facades\File;
 
 class FlipgridController extends Controller
 {
+    protected $payload;
+
     public function  index() {
         $flipgrids = Flipgrid::all();
         return view('flipgrids.index', [
@@ -17,39 +19,34 @@ class FlipgridController extends Controller
 
     public  function create(Request $request) {
         try {
-            $payload = json_decode($request->getContent());
-            if (empty($payload)) {
-                return response()->json('Missing content', 200);
-            }
-
-            if(property_exists($payload, 'Type') &&
-                $payload->Type === "SubscriptionConfirmation") {
-                $confirmation_url = curl_init($payload->SubscribeURL);
-                curl_exec($confirmation_url);
-                return response()->json( 'success', 200);
-            }
-            if(property_exists($payload, 'Message')) {
-                $data = json_decode($payload->Message);
-                $fg = new Flipgrid;
-                $fg->grid_id = $data->grid_id;
-                $fg->topic_id = $data->topic_id;
-                $fg->payload = json_encode($request->getContent());
-                $fg->save();
-                return response()->json( 'Message received', 200);
-            }
+            return $this->processRequest($request);
         } catch (\Exception $e) {
-            $fg = new Flipgrid;
-            $fg->grid_id = 1;
-            $fg->topic_id = 1;
-            $fg->payload = json_encode($e->getMessage());
-            $fg->save();
             return response()->json('Error'.$e->getMessage(), 500);
         }
-        return response()->json('Warning, nothing happened', 200);
     }
 
-    public function getConfirmation() {
-        $data = File::get('confirmation.txt');
-        return response()->json($data, 200);
+    public function processRequest(Request $request) {
+        $this->payload = null;
+        $payload = json_decode($request->getContent());
+        if (empty($payload)) {
+            return response()->json('Missing content', 200);
+        }
+
+        if(property_exists($payload, 'Type') && $payload->Type === "SubscriptionConfirmation") {
+            $confirmation_url = curl_init($payload->SubscribeURL);
+            curl_exec($confirmation_url);
+            return response()->json( 'success', 200);
+        }
+
+        if(property_exists($payload, 'Message')) {
+            $this->payload = json_decode($payload->Message);
+            $fg = new Flipgrid;
+            $fg->grid_id = $this->payload->grid_id;
+            $fg->topic_id = $this->payload->topic_id;
+            $fg->payload = json_encode($payload->Message);
+            $fg->save();
+            return response()->json( 'Message received', 200);
+        }
+        return response()->json('Warning, nothing happened', 200);
     }
 }
