6d1413169bb2d5df3efe0ca74341e59785d813ed
Timothy Wikedzi <timothy.wikedzi@andela.com>
2022-02-16 13:02:59 +0300

    update controller

--- app/Http/Controllers/FlipgridController.php
+++ app/Http/Controllers/FlipgridController.php
@@ -36,16 +36,7 @@ class FlipgridController extends Controller
 
     public function processRequest(Request $request) {
         $this->payload = null;
-        ///$payload = json_decode($request->getContent());
-        $payload = "==========";
-        $fg = new Flipgrid;
-        $fg->completedAt = '';
-        $fg->fgResponseID = '';
-        $fg->fgQuestionID = '';
-        $fg->fgGridID = '';
-        $fg->payload = $payload."===";
-        $fg->save();
-
+        $payload = json_decode($request->getContent());
         if (empty($payload)) {
             return response()->json('Missing content', 200);
         }
@@ -58,24 +49,13 @@ class FlipgridController extends Controller
 
         if(property_exists($payload, 'Message')) {
             $this->payload = json_decode($payload->Message);
-            $fg = new Flipgrid;
-            $fg->completedAt = '';
-            $fg->fgResponseID = '';
-            $fg->fgQuestionID = '';
-            $fg->fgGridID = '';
-            $fg->payload = $payload;
-            $fg->save();
-            return response()->json('Error', 500);
-
             if (!empty($this->payload) && $this->payload->data) {
-                $data = $this->payload->data;
-                $content = $data->content;
-                $gridID = $content->grid->id;
+                $gridID = $this->payload->data->grid->id;
                 $gid = intval($gridID);
                 $fg = new Flipgrid;
-                $fg->completedAt = Carbon::parse($content->response->created_at)->toDateTimeString();
-                $fg->fgResponseID = $content->response->id;
-                $fg->fgQuestionID = $content->response->topic_id;
+                $fg->completedAt = Carbon::parse($this->payload->data->response->created_at)->toDateTimeString();
+                $fg->fgResponseID = $this->payload->data->response->id;
+                $fg->fgQuestionID = $this->payload->data->response->topic_id;
                 $fg->fgGridID = $gridID;
                 $fg->payload = $payload->Message;
                 if ($gid % 2 == 0 && env('TYPE') == 'even') {
